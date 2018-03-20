64e552d319c93a63afee9354403bb69890e04206
pardeeprj90 <37590015+pardeeprj90@users.noreply.github.com>
2018-03-20 21:54:13 +0530

    V1.0
    Source Code upload

---
+++ JavaDay9_WrapperClasses.java
@@ -0,0 +1,17 @@
+package javaPractice_10Feb2018;
+
+public class JavaDay9_WrapperClasses {
+
+	public static void main(String[] args) {
+		String x = "12.23";
+		System.out.println(x + 20);
+		
+		Double i=Double.parseDouble(x);
+		System.out.println(i);
+		
+		int j=10;
+		String s=String.valueOf(j);
+		System.out.println(s);
+	}
+
+}
